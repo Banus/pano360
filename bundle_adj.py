Fix symbolic Jacobian and bad matches, add timing
Fixed missing missing accumulation in symbolic Jacobian computation,
now working; remove image pairs with bad RMSE to avoid diverging BA.
Added timings for keypoints, matching, BA and blending.

bundle_adj.py
@@ -10,7 +10,9 @@ PARAMS_PER_CAMERA = 6
 TERMS_PER_MATCH = 2
 # Levenberg–Marquardt parameters
 LM_LAMBDA = 5         # regularization strenght
-LM_MAX_ITER = 10      # maximum number of iterations
+LM_MAX_ITER = 100     # maximum number of iterations
+# remove matches with this initial error - they are likely mismatches
+MIN_MATCH_ERROR = 150
 
 
 @dataclass
@@ -140,13 +142,16 @@ def camera_to_params(camera):
     return np.concatenate([params, mat_to_angle(camera.rot)])
 
 
+def get_diff(cam1, cam2, match):
+    """Get the residual for a given match."""
+    hom = _hom_to_from(cam1, cam2)
+    trans = hom.dot(match[:, 3:6].T)
+    return (match[:, :3].T - trans / trans[[-1], :])[:-1].ravel()
+
+
 def residuals(cameras, matches):
     """Find estimation errors."""
-    res = []
-    for i, j, match in matches:
-        hom = _hom_to_from(cameras[j], cameras[i])
-        trans = hom.dot(match[:, 3:6].T)
-        res.append((match[:, :3].T - trans / trans[[-1], :])[:-1].ravel())
+    res = [get_diff(cameras[j], cameras[i], m) for i, j, m in matches]
     return np.concatenate(res, axis=0)
 
 
@@ -191,7 +196,7 @@ def _jacobian_symbolic(cameras, matches):
 
     # cache rotation derivatives
     drs = [dr_dvi(cameras[i].rot) for i in cam_idx]
-    for idx, (i, j, match) in enumerate(matches):
+    for idx, (j, i, match) in enumerate(matches):
         m_slice = slice(m_offs[idx]*TERMS_PER_MATCH,
                         m_offs[idx+1]*TERMS_PER_MATCH)
 
@@ -241,14 +246,14 @@ def _jacobian_symbolic(cameras, matches):
         i_slice = slice(c_off_i, c_off_i+PARAMS_PER_CAMERA)
         j_slice = slice(c_off_j, c_off_j+PARAMS_PER_CAMERA)
 
-        jac_t_jac[i_slice, i_slice] = \
+        jac_t_jac[i_slice, i_slice] += \
             jac[m_slice, i_slice].T.dot(jac[m_slice, i_slice])
-        jac_t_jac[j_slice, j_slice] = \
+        jac_t_jac[j_slice, j_slice] += \
             jac[m_slice, j_slice].T.dot(jac[m_slice, j_slice])
 
         cross_block = jac[m_slice, i_slice].T.dot(jac[m_slice, j_slice])
-        jac_t_jac[i_slice, j_slice] = cross_block
-        jac_t_jac[j_slice, i_slice] = cross_block.T
+        jac_t_jac[i_slice, j_slice] += cross_block
+        jac_t_jac[j_slice, i_slice] += cross_block.T
 
     return jac, jac_t_jac
 
@@ -290,12 +295,15 @@ class IncrementalBundleAdjuster:
         self.mode = mode
 
     def add(self, idx, camera, matches):
-        """Add a new camerato the bundler."""
+        """Add a new camera to the bundler."""
         self.cameras[idx] = camera
         for new, cam in enumerate(self.cameras):
             if cam is None or new not in matches[idx]:
                 continue
-            self.matches.append((new, idx, matches[idx][new][0]))
+            match = matches[idx][new][0]
+            if loss(get_diff(camera, cam, match)) > MIN_MATCH_ERROR:
+                continue
+            self.matches.append((new, idx, match))
 
         if self.mode == "incr":
             self.optimize()
@@ -311,8 +319,7 @@ class IncrementalBundleAdjuster:
         n_not_improved = 0   # exit loop if the loss doesn't improve
         for it_ in range(LM_MAX_ITER):
             # Levenberg–Marquardt iteration
-            jac, jac_t_jac = _jacobian_numeric(self.cameras, self.matches)
-
+            jac, jac_t_jac = _jacobian_symbolic(self.cameras, self.matches)
             bb_ = jac.T.dot(errs)
             jac_t_jac += np.eye(jac.shape[1]) * LM_LAMBDA
 
@@ -327,7 +334,7 @@ class IncrementalBundleAdjuster:
 
             errs = residuals(cams, self.matches)
             err = loss(errs)
-            if err < best_err - 1e-6:
+            if err < best_err - 1e-3:
                 best_err = err
                 self.cameras = cams
             else:

features.py
@@ -6,6 +6,7 @@ import argparse
 import logging
 import math
 import os
+import time
 from collections import defaultdict
 
 import numpy as np
@@ -243,7 +244,7 @@ def _match_hom(pt1, pt2, des1, des2):
     hom, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC)
     mask = (mask != 0).squeeze()    # boolean mask
 
-    return match[mask].squeeze(), hom
+    return match[mask, :], hom
 
 
 def _reverse(match, hom):
@@ -254,6 +255,7 @@ def _reverse(match, hom):
 def matching(imgs, detect=sift_detector()):
     """Find correspondences between images in a list."""
     kpts, descs = [], []
+    start = time.time()
     for i, img in enumerate(imgs):
         logging.debug(f"Processing image #{i+1}")
 
@@ -261,9 +263,11 @@ def matching(imgs, detect=sift_detector()):
         cent = np.array([img.shape[1], img.shape[0]]) / 2
         kpts.append(np.float32([kp.pt - cent for kp in kp_]))
         descs.append(des)
+    logging.info(f"Extracted keypoints, time: {time.time() - start}")
 
     # match all possible pairs
     matches, n_imgs = defaultdict(dict), len(imgs)
+    start = time.time()
     for src in range(n_imgs):
         for dst in range(src+1, n_imgs):
             logging.debug(f"Matching {src+1}-{dst+1}")
@@ -274,6 +278,7 @@ def matching(imgs, detect=sift_detector()):
 
             matches[src][dst] = (match, hom)
             matches[dst][src] = _reverse(match, hom)
+    logging.info(f"Matched features, time: {time.time() - start}")
 
     return np.array(kpts, dtype=np.object), np.array(matches, dtype=np.object)
 

stitcher.py
@@ -6,6 +6,7 @@ import argparse
 import logging
 import os
 import pickle
+import time
 
 import numpy as np
 import cv2
@@ -146,7 +147,8 @@ def estimate_resolution(regions):
 
     mid = regions[len(regions) // 2]   # central image
     im_shape = np.array(mid.img.shape[:2][::-1])
-    resolution = (mid.range[1] - mid.range[0]) / im_shape
+    mid_range = _proj_img_range_corners(mid.img.shape[:2], mid.hom())
+    resolution = (mid_range[1] - mid_range[0]) / im_shape
 
     max_side = np.max(size / resolution)
     if max_side > MAX_RESOLUTION:
@@ -369,8 +371,8 @@ def crop_mosaic(mosaic, valid):
 
 def idx_to_keypoints(matches, kpts):
     """Replace keypoint indices with their coordinates."""
-    def _i_to_k(matches, kpt1, kpt2):
-        return np.concatenate([kpt1[matches[:, 0]], kpt2[matches[:, 1]]],
+    def _i_to_k(match, kpt1, kpt2):
+        return np.concatenate([kpt1[match[:, 0]], kpt2[match[:, 1]]],
                               axis=1)
 
     # homogeneous coordinates
@@ -388,7 +390,8 @@ def idx_to_keypoints(matches, kpts):
 def main():
     """Script entry point."""
     parser = argparse.ArgumentParser(description="Stitch images.")
-    parser.add_argument('--path', type=str, default="../data/ppwwyyxx/uav",
+    parser.add_argument('-p', '--path', type=str,
+                        default="../data/ppwwyyxx/NSH",
                         help="directory with the images to process.")
     parser.add_argument("-s", "--shrink", type=float, default=2,
                         help="downsample the images by this amount.")
@@ -429,13 +432,17 @@ def main():
         with open(f"ba_{name}.pkl", 'rb') as fid:
             regions = pickle.load(fid)
     except IOError:
+        start = time.time()
         regions = traverse(imgs, idx_to_keypoints(matches, kpts),
                            badjust=args.ba)
+        logging.info(f"Image registration, time: {time.time() - start}")
         with open(f"ba_{name}.pkl", 'wb') as fid:
             pickle.dump(regions, fid, protocol=pickle.HIGHEST_PROTOCOL)
 
+    start = time.time()
     mosaic = stitch(regions, blender=BLENDERS[args.blend],
                     equalize=args.equalize, crop=args.crop)
+    logging.info(f"Built mosaic, time: {time.time() - start}")
 
     if args.out:
         cv2.imwrite(args.out, mosaic)